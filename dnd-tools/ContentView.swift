54578762280b0a2717c690393dbb99ecd46a9b83
Brandodn Assing <assing.brandon@gmail.com>
2022-02-08 15:34:46 -0500

    Created project with .gitignore

---
+++ dnd-tools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dnd-tools
+//
+//  Created by Brandon Assing on 2022-02-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dnd-tools/dnd_toolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  dnd_toolsApp.swift
+//  dnd-tools
+//
+//  Created by Brandon Assing on 2022-02-08.
+//
+
+import SwiftUI
+
+@main
+struct dnd_toolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
