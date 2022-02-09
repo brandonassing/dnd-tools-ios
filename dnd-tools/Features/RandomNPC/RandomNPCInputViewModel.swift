e34696fcc094f73b56be93e6e3176d58001400b8
Brandodn Assing <assing.brandon@gmail.com>
2022-02-09 09:55:58 -0500

    Set name to be received from name repo

--- dnd-tools/Features/RandomNPC/RandomNPCInputViewModel.swift
+++ dnd-tools/Features/RandomNPC/RandomNPCInputViewModel.swift
@@ -5,6 +5,7 @@ import SwiftUI
 extension RandomNPC {
 	class InputViewModel: ObservableObject, ViewModel {
 		typealias Dependencies = HasNameRepository
+		typealias NPCInfo = (heightCm: Int, bodyType: BodyType, race: Race, ageGroup: AgeGroup, gender: Gender)
 		
 		struct Inputs {
 			let generate: PassthroughSubject<Void, Never>
@@ -23,8 +24,8 @@ extension RandomNPC {
 				generate: generateSubject
 			)
 			
-			let npc = generateSubject
-				.map({ _ -> NPC? in
+			let npcInfo = generateSubject
+				.map({ _ -> NPCInfo? in
 					let race = Race.allCases.randomElement() ?? .human
 					let ageGroup = AgeGroup.allCases.randomElement() ?? .adult
 					let bodyType = BodyType.allCases.randomElement() ?? .average
@@ -38,11 +39,47 @@ extension RandomNPC {
 					} else if ageGroup == .infant {
 						height = height * 0.5
 					}
-					return NPC(name: "Bob Vance", heightCm: Int(height), bodyType: bodyType, race: race, ageGroup: ageGroup, gender: gender)
+					return (heightCm: Int(height), bodyType: bodyType, race: race, ageGroup: ageGroup, gender: gender)
+				})
+			
+			let npcNameRequest = npcInfo
+				.map({ npcInfo -> AnyPublisher<Result<String, Error>, Never> in
+					guard let npcInfo = npcInfo else {
+						return Empty<Result<String, Error>, Never>()
+							.eraseToAnyPublisher()
+					}
+					return dependencies.nameRepository.getRandomName(for: npcInfo.race, npcInfo.gender)
+				})
+				.switchToLatest()
+			
+			// TODO: handle success/failure parsing better
+			let npcNameSuccess = npcNameRequest
+				.filter({
+					switch $0 {
+					case .success:
+						return true
+					case .failure:
+						return false
+					}
+				})
+				.map({ result -> String? in
+					switch result {
+					case .success(let name):
+						return name
+					case .failure:
+						return nil
+					}
+				})
+			
+			let npc = npcInfo.zip(npcNameSuccess, { (npcInfo, name) -> NPC? in
+					guard let npcInfo = npcInfo, let name = name else {
+						return nil
+					}
+					return NPC(name: name, heightCm: npcInfo.heightCm, bodyType: npcInfo.bodyType, race: npcInfo.race, ageGroup: npcInfo.ageGroup, gender: npcInfo.gender)
 				})
 				.receive(on: DispatchQueue.main)
 				.eraseToAnyPublisher()
-			
+
 			self.outputs = Outputs(
 				npc: npc
 			)
