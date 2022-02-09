204bb833f23d6d8b02593d29c3b411e1b2dfbfcf
Brandodn Assing <assing.brandon@gmail.com>
2022-02-09 09:12:04 -0500

    Added character type builder for API value generation based on race and gender

--- dnd-tools/Respositories/NameRepository.swift
+++ dnd-tools/Respositories/NameRepository.swift
@@ -8,8 +8,15 @@ protocol NameRepository {
 class CharacterNameRepository: NameRepository {
 	typealias Dependencies = HasNameService
 	
+	private func buildCharacterType(for race: Race, _ gender: Gender) -> String {
+		if race == .tiefling {
+			return race.apiValue
+		}
+		return "\(race.apiValue)+\(gender.apiValue)"
+	}
+	
 	func getRandomName(for race: Race, _ gender: Gender) -> AnyPublisher<Result<String, Error>, Never> {
-		return self.nameService.getRandomCharacterName(characterType: "Human+Male", limit: 1)
+		return self.nameService.getRandomCharacterName(characterType: self.buildCharacterType(for: race, gender), limit: 1)
 			.map({ result in
 				if case .success(let names) = result, let name = names.first {
 					return .success(name)
@@ -23,5 +30,41 @@ class CharacterNameRepository: NameRepository {
 	
 	init(dependencies: Dependencies) {
 		self.nameService = dependencies.nameService
+	}
+}
+
+fileprivate extension Race {
+	var apiValue: String {
+		switch self {
+		case .human:
+			return "Human"
+		case .halfling:
+			return "Halfling"
+		case .dwarf:
+			return "Dwarvish"
+		case .elf:
+			return "Elvish"
+		case .gnome:
+			return "" // TODO
+		case .dragonborn:
+			return "Draconic"
+		case .halfElf:
+			return ["Elvish", "Human"].randomElement() ?? "Elvish"
+		case .halfOrc:
+			return ["Orcish", "Human"].randomElement() ?? "Orcish"
+		case .tiefling:
+			return "Fiendish"
+		}
+	}
+}
+
+fileprivate extension Gender {
+	var apiValue: String {
+		switch self {
+		case .male, .female:
+			return self.name
+		case .nonBinary:
+			return ([Gender.male, Gender.female].randomElement() ?? Gender.male).name
+		}
 	}
 }
