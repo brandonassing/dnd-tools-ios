3e621612c25ed4042d6758a74d9cc86294e75af9
Brandodn Assing <assing.brandon@gmail.com>
2022-02-11 22:41:27 -0500

    Added loot generator UI and API connection

--- dnd-tools/Features/LootGenerator/LootGenerationView.swift
+++ dnd-tools/Features/LootGenerator/LootGenerationView.swift
@@ -2,8 +2,30 @@
 import SwiftUI
 
 struct LootGenerationView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+	
+	@StateObject private var viewModel = LootGenerationViewModel(dependencies: GlobalDependencyContainer.shared)
+	@State private var loot: String?
+	
+	var body: some View {
+		
+		VStack(spacing: 0) {
+			
+			if let loot = self.loot {
+				Text(loot)
+					.textStyle(.standard)
+			}
+
+			Spacer()
+			
+			PrimaryButtonView(text: "Generate", action: { self.viewModel.inputs.generate.send() })
+				.onReceive(self.viewModel.outputs.loot) { loot in
+					self.loot = loot
+				}
+		}
+		.padding()
+		.navigationTitle("Loot Generator")
+		.background(StyleGuide.Color.background)
+		
     }
 }
 
--- dnd-tools/Services/LootService/LootAPIService.swift
+++ dnd-tools/Services/LootService/LootAPIService.swift
@@ -1,4 +1,30 @@
 
+import Foundation
+import Combine
+import CombineExt
+
 class LootAPIService: LootService {
+	
+	private let session = URLSession.shared
+	private let baseUrl = "https://donjon.bin.sh/fantasy/random/rpc-fantasy.fcgi"
+	
+	func getRandomLoot(limit: Int) -> AnyPublisher<Result<[String], Error>, Never> {
+		guard let url = URL(string: self.baseUrl + "?type=Purse&n=\(limit)") else {
+			return Just(Result.failure(GenericError.apiError))
+				.eraseToAnyPublisher()
+		}
+		
+		return self.session.dataTaskPublisher(for: url)
+			.tryMap({ data, response in
+				if let response = response as? HTTPURLResponse,
+				   !(200...299).contains(response.statusCode) {
+					throw GenericError.apiError
+				}
+				return data
+			})
+			.decode(type: [String].self, decoder: JSONDecoder())
+			.mapToResult()
+			.eraseToAnyPublisher()
+	}
 	
 }
